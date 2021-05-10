7947f323a3cd5e501e3c75343962088386a2bf93
ncgege <8160903@qq.com>
2021-05-10 11:11:19 +0800

    Update install-megacli.sh

--- install-megacli.sh
+++ install-megacli.sh
@@ -4,4 +4,4 @@ cd ~
 wget https://docs.broadcom.com/docs-and-downloads/raid-controllers/raid-controllers-common-files/8-07-06_MegaCLI.zip
 mkdir ~/MegaCLI   &&  unzip -d ~/MegaCLI  ~/8-07-06_MegaCLI.zip
 alien  -k --scripts ~/MegaCLI/Linux/MegaCli-8.07.06-1.noarch.rpm
-dpkg -i ~/MegaCLI/Linux/megacli_8.07.06-1_all.deb
+dpkg -i ./megacli_8.07.06-1_all.deb
